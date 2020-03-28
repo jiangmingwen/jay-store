2c972d3aad7990fe5eef9cb7796276b5b96e3d9e
chris.xiao <chris.xiao@topibd.com>
2020-03-28 19:45:39 +0800

    aa

---
+++ store-admin/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '@/views/Login'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'',
+    redirect:'/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
